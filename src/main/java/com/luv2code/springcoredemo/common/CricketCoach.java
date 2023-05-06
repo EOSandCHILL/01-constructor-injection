89565e9a356f6814a72014f1bd654a7001d1f503
MackInTech <jerrykmack803@yahoo.com>
2023-05-06 08:06:55 -0400

    02-springboot-core

--- src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -1,4 +1,4 @@
-package com.luv2code.springcoredemo;
+package com.luv2code.springcoredemo.common;
 import org.springframework.stereotype.Component;
 
 @Component // Marks the class as a spring bean which means it's now available for dependency injection.
--- src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -1,4 +1,5 @@
-package com.luv2code.springcoredemo;
+package com.luv2code.springcoredemo.rest;
+import com.luv2code.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
